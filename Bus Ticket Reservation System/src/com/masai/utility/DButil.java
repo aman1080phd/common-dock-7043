ced0914006c60dbfdfcc9248ca53ac9b594e2e73
AmanKumar33712 <103635237+AmanKumar33712@users.noreply.github.com>
2022-09-29 16:33:46 +0530

    DAO created

---
+++ Bus Ticket Reservation System/src/com/masai/utility/DButil.java
@@ -0,0 +1,39 @@
+package com.masai.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DButil {
+	
+public static Connection provideConnection() {
+		
+		Connection conn=null;
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String url="jdbc:mysql://localhost:3306/web19sb101db";
+		
+		try {
+			conn= DriverManager.getConnection(url,"root","root");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		return conn;
+		
+	}
+	
+	
+
+}
